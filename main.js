173564482244eb5537c131a487df8fdbacd5415a
laryssaGab <134020957+laryssaGab@users.noreply.github.com>
2024-10-21 21:42:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
@@ -42,6 +43,7 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
+       
     for (let i=0; i<contadores.length;i++){
         contadores[i].textContent = calculaTempo(tempos[i]);   
     }
@@ -52,4 +54,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-comecaCronometro();
+//comecaCronometro();
